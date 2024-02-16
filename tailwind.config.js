dc37d6feeb4b8284a4b61c2030942077f92bce89
Marcel Aguiar <marcelaguiar9@gmail.com>
2024-02-15 19:47:53 -1000

    Color palette, nav bar item active styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-dark': '#121113',
+        'primary-light': '#FAF7EF',
+        'primary': '#4BB3FD',
+        'secondary': '#246A73',
+        'tertiary': '#E55934',
+      },
+    },
   },
   plugins: [],
 }
