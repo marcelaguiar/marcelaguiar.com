b1f10a2043bcd4a6cc7a859e90398c9ea730ed49
Marcel Aguiar <marcelaguiar9@gmail.com>
2024-02-18 14:56:16 -1000

    Mobile menu clean up with transitions

--- src/static/custom/js/base.js
+++ src/static/custom/js/base.js
@@ -1,6 +1,6 @@
 // Icons
-const sunIcon = document.querySelector(".sun");
-const moonIcon = document.querySelector(".moon");
+const sunIcons = document.querySelectorAll(".sun");
+const moonIcons = document.querySelectorAll(".moon");
 
 const mobileMenuBtn = document.querySelector("button.mobile-menu-button");
 const mobileMenu = document.querySelector(".mobile-menu");
@@ -11,19 +11,27 @@ const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 // Icon Toggling
 const iconToggle = () => {
-    moonIcon.classList.toggle("display-none");
-    sunIcon.classList.toggle("display-none");
+    for (let icon of moonIcons) {
+        icon.classList.toggle("display-none");
+    }
+    for (let icon of sunIcons) {
+        icon.classList.toggle("display-none");
+    }
 };
 
 // Initial Theme Check
 const themeCheck = () => {
     if(userTheme === "dark" || (!userTheme && systemTheme)) {
         document.documentElement.classList.add("dark");
-        moonIcon.classList.add("display-none");
+        for (let icon of moonIcons) {
+            icon.classList.add("display-none");
+        }
         return;
     }
 
-    sunIcon.classList.add("display-none");
+    for (let icon of sunIcons) {
+        icon.classList.add('display-none');
+    }
 };
 
 // Manual Theme Switch
@@ -41,19 +49,37 @@ const themeSwitch = () => {
 };
 
 // call theme switch on clicking buttons
-sunIcon.addEventListener("click", () => {
-    themeSwitch();
+sunIcons.forEach(function(icon) {
+    icon.addEventListener("click", () => {
+        themeSwitch();
+    });
 });
-
-moonIcon.addEventListener("click", () => {
-    themeSwitch();
+moonIcons.forEach(function(icon) {
+    icon.addEventListener("click", () => {
+        themeSwitch();
+    });
 });
 
 // invoke theme check on initial load
 themeCheck();
 
-
-
 mobileMenuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
+    let h = mobileMenu.style.maxHeight;
+    if(h == "0px") {
+        showMobileMenu();
+    }
+    else {
+        hideMobileMenu();
+    }
+    
 });
+
+function hideMobileMenu() {
+    mobileMenu.style.maxHeight = "0px";
+    mobileMenu.style.borderWidth = "0px";
+}
+
+function showMobileMenu() {
+    mobileMenu.style.maxHeight = "400px";
+    mobileMenu.style.borderWidth = "1px";
+}
