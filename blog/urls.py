c4165af5db160ba3f248a4f726ce925a1771a979
Marcel Aguiar <marcelaguiar9@gmail.com>
2024-03-14 22:41:54 -1000

    Starting newsletter subscription

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.blog_home, name="blog-home"),
     path('latest/feed/', LatestEntriesFeed(), name="rss-feed"),
     path('post/<int:id>/<slug:slug>/', views.blog_post, name="blog-post"),
+    path('email_subscribe/', views.email_subscribe, name="email-subscribe"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from blog.models import Post
 
+from .forms import EmailForm
+
 
 def blog_home(request):
     all_blog_posts = Post.objects.all().order_by('-created')
@@ -17,3 +19,14 @@ def blog_post(request, id, slug):
         'blog_post': blog_post
     }
     return render(request, 'blog/post.html', context)
+
+def email_subscribe(request):
+    if request.method == "POST":
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect("/thanks/")
+
+    return render(request, "name.html", {"form": form})
