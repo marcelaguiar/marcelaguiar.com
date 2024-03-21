f5962b7cce0f5274eae65708c8e86662c9b5f1a6
Marcel Aguiar <marcelaguiar9@gmail.com>
2024-03-20 21:57:05 -1000

    Newsletter subscription with reCaptcha

--- static/custom/js/blog_base.js
+++ static/custom/js/blog_base.js
@@ -1,3 +1,7 @@
+function subscribe(token) {
+    $("#post-form").submit();
+}
+
 $(document).on('submit','#post-form', function(e) { 
     e.preventDefault();
     $.ajax({
@@ -9,7 +13,13 @@ $(document).on('submit','#post-form', function(e) {
         },
         success: function(data) {
             if(data.success) {
-                alert("success");
+                // Success message
+                let timeoutId = 0;
+                toggleSubscribe();
+                timeoutId = setTimeout(function() {
+                    toggleSubscribe();
+                    $('#subscriber_email').val("");
+                }, 2000);
             }
             else {
                 alert("error");
@@ -19,4 +29,28 @@ $(document).on('submit','#post-form', function(e) {
             alert(errorThrown)
         }
       });
-})  
+}) 
+
+
+let subscribeButton = document.getElementById("subscribe-button");
+let subscribeConfirmation = document.getElementById("subscription-confirmation");
+let subContainer = document.getElementById("subscribe-container");
+
+function toggleSubscribe() {
+    if(subscribeButton.style.display == "none") {
+        switchToSubscribeButton();
+    }
+    else {
+        switchToSuccessIcon();
+    }    
+}
+
+function switchToSubscribeButton() {
+    subscribeConfirmation.style.display = "none";
+    subscribeButton.style.display = "block";
+}
+
+function switchToSuccessIcon() {
+    subscribeButton.style.display = "none";
+    subscribeConfirmation.style.display = "block";
+}
